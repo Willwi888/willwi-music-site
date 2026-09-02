4149646c7378a9959097658ed93faa25cc3ece40
Willwi <willchean@gmail.com>
2026-09-02 12:35:52 +0800

    stop intercepting audio requests

--- sw.js
+++ sw.js
@@ -1,3 +1,2 @@
 self.addEventListener('install',event=>{self.skipWaiting();});
-self.addEventListener('activate',event=>{event.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.map(k=>caches.delete(k)));await self.clients.claim();const list=await self.clients.matchAll({type:'window'});list.forEach(c=>c.navigate(c.url.split('?')[0]+'?v=field'));})());});
-self.addEventListener('fetch',event=>{event.respondWith(fetch(event.request,{cache:'no-store'}));});
+self.addEventListener('activate',event=>{event.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.map(k=>caches.delete(k)));await self.clients.claim();})());});
